fd998dd4d3bd8178b05625a1bbd0ffb2833a4661
Alla Bulgakova <co.alla.ios@gmail.com>
2026-03-24 13:09:14 +0200

    Add Firebase integration

--- MathTrainer/MathTrainer/AppDelegate.swift
+++ MathTrainer/MathTrainer/AppDelegate.swift
@@ -5,20 +5,20 @@
 //  Created by Macbook Pro on 11/13/24.
 //
 
+import FirebaseCore
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
